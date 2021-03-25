aceedbb349638a33f944fb01c672061508585558
vborys <valeriy.borys@gmail.com>
2021-03-25 14:54:49 +0300

    unusable imports deleted

--- src/main/java/com/example/sec/config/SpringSecurityConfig.java
+++ src/main/java/com/example/sec/config/SpringSecurityConfig.java
@@ -1,20 +1,17 @@
 package com.example.sec.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 import javax.annotation.Resource;
 
@@ -39,23 +36,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter implement
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authProvider());
     }
 
-
-    //    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .anyRequest().hasRole("USER")
-//                .and().formLogin().and()
-//                .httpBasic()
-//                .and()
-//                .logout()
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/")
-//        ;
-//    }
 }
